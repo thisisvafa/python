23ce22f17dc1fd3a1113959107a8b54108143d26
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-08 15:46:35 +0330

    lesson 2

---
+++ lesson_2.py
@@ -0,0 +1,21 @@
+# Operators: + - * /
+# **
+
+print(3 ** 3) # means: 3 power of 2
+print(3 ** 6)
+print(3 ** 9)
+print(369 ** 0.9)
+
+print('------------------------------')
+
+# % return what remains from division
+print(68 / 6)
+print(68 % 6)
+print(78 % 2)
+
+print('------------------------------')
+
+# // return integer part of a division
+print((68 - (68 % 6)) / 6)
+print(68 // 6)
+
