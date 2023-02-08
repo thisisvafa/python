9e9b952fb5f7e197776612a6c49775216e58dd75
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-08 15:22:15 +0330

    lesson 1

---
+++ main.py
@@ -0,0 +1,21 @@
+print('hello world')
+
+print(type(5))
+print(type(5.3))
+print(type(5j))
+print(type('Hello'))
+
+print('----------------------------------')
+
+print(3 + 9)
+print(3 + 6.9)
+print(3.9 - 9.6)
+
+print('----------------------------------')
+
+print(6 + 9)
+print(6 - 9)
+print(6 * 9)
+print(6 / 9)
+
+print((3 + 9) * 6 / 9)
