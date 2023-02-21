bf95729a2463ca5ae6e9819e046a6f48cd3a516d
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-21 15:51:31 +0330

    lesson 6

---
+++ lesson_6.py
@@ -0,0 +1,22 @@
+userName = 'Vafa'
+userFamily = 'Pour'
+
+result = "user name is " + userName + ' and user family is ' + userFamily
+print(result)
+
+result2 = f'user name is {userName} and user family is {userFamily}'
+print(result2)
+
+
+print('----------------------------------------------')
+
+
+number_1 = 3
+number_2 = 6
+print(f"sum is {number_1 + number_2}")
+
+
+print('----------------------------------------------')
+
+# string indexes
+print(userName[0])
