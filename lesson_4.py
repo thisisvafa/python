8e9632dc61ea15e6fc0f770f52a4e1f29719c02c
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-21 15:27:22 +0330

    lesson 4

---
+++ lesson_4.py
@@ -0,0 +1,24 @@
+productPrice = 100000 # Integer
+productName = "Laravel Course" # String
+isRecording = False # Boolean
+productTag = ['Laravel', 'Laravel Course', 'free laravel course', 9] # List
+product = { "productPrice": 100000, "productName": "Laravel Course" }
+
+print(type(productPrice))
+print(type(productName))
+print(type(isRecording))
+print(type(productTag))
+print(productTag)
+print(type(product))
+print(product)
+
+
+print('----------------------------------------')
+
+
+personName = "Vafa"
+personFamily = "Pour"
+personChild = None # None Means Empty
+
+print(personChild)
+print(type(personChild))
