08bd2f50adf82876b9f8802509e770ff2235bb25
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-21 13:52:06 +0330

    lesson 3

---
+++ lesson_3_0.py
@@ -0,0 +1,24 @@
+x = 100
+y = 200
+# x = 50
+x = y
+print(x + y)
+
+
+print('------------------------------')
+
+
+w, z = 400, 500
+print(w)
+
+
+print('------------------------------')
+
+
+_myNumber = 100
+_myNumber = _myNumber -2
+print(_myNumber)
+_myNumber2 = 50
+_mynumber2 = 48
+print(_myNumber2)
+print(_mynumber2)
---
+++ lesson_3_1.py
@@ -0,0 +1,10 @@
+my_age = 27 # snake case variable naming
+
+myAge = 27 # camel case variable naming
+
+
+PI = 3.14
+
+myAge = 27
+
+__no_touch__ = 2
