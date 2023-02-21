61868da1895b7ff826ab753eddd547fa1563a40a
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-21 15:38:29 +0330

    lesson 5

---
+++ lesson_5_0.py
@@ -0,0 +1,14 @@
+# sentence = 'Vafa said: "I\'m programmer"'
+# sentence = 'Vafa said:\n"I\'m programmer"'
+sentence = 'Vafa said:\t"I\'m programmer"'
+
+print(sentence)
+
+
+print('-------------------------------------------')
+
+
+myName = "Vafa"
+myFamily = "Pour"
+
+print(myName + ' ' + myFamily)
---
+++ lesson_5_1.py
@@ -0,0 +1,9 @@
+number = 3
+
+print(number)
+
+# number += 5 # -> number = number + 5
+# number -= 5 # -> number = number - 5
+# xnumber *= 5 # -> number = number * 5
+number /= 5 # -> number = number / 5
+print(number)
