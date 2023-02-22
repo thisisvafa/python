2be027ce5a6e922af2addd32a1938f67a4e5e7bf
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-22 15:43:40 +0330

    lesson 7

--- lesson_7.py
+++ lesson_7.py
@@ -1,2 +1,14 @@
-age=int(input("tell me your age"))
-print(age)
+# age=int(input("tell me your age"))
+# print(age)
+
+
+from tkinter import*
+
+# parsa=Tk()
+page=Tk()
+page.geometry('400x400')
+p1 = PhotoImage(file = 'icon.png')
+page.iconphoto(False, p1)
+page.title("afarin")
+page.config(bg="#FDF854")
+mainloop()
