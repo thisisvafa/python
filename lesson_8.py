70974a355c4050cf45b64a435afb83d6ff961cd4
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-25 15:11:45 +0330

    lesson 8

---
+++ lesson_8.py
@@ -0,0 +1,12 @@
+#interpolation
+print(f"multiply 2 and 6 is: { 2 * 6 }")
+
+# myWeight = 80
+myWeight = 80.600
+print(myWeight)
+
+myWeight = int(myWeight)
+print(myWeight)
+
+myWeight = str(myWeight)
+print(myWeight)
