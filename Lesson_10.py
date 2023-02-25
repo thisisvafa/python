9c395a9ed8748460b8fe2cb3bd2d2daa8468c3ab
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-25 15:49:59 +0330

    lesson 10

---
+++ Lesson_10.py
@@ -0,0 +1,13 @@
+# Condition
+# print("Enter your medal?")
+
+userRank = int(input("Enter your medal? \n"))
+
+if userRank == 1:
+    print("You get GOLD medal")
+elif userRank == 2:
+    print("You get SILVER medal")
+elif userRank == 3:
+    print("You get BRONZE medal")
+else:
+    print("You get no medal")
