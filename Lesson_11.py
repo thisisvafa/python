ddf4c3a86ed690851ac103eb0fc63001749803c8
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-02-25 15:54:35 +0330

    lesson 11

---
+++ Lesson_11.py
@@ -0,0 +1,14 @@
+List_1 = ['a', 'b', 'c']
+List_2 = List_1
+List_3 = list(List_1)
+
+print(List_1)
+print(List_2)
+print(List_3)
+
+
+print('------------------------------')
+
+
+print(List_1 is List_2)
+print(List_1 is List_3)
