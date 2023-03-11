94a3ba15f34534aa50e52a34b7a72ce11ce5c612
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-03-11 13:36:35 +0330

    lesson 12

---
+++ Lesson_12.py
@@ -0,0 +1,28 @@
+number_1 = 2
+number_2 = 1
+
+# == : return true if the value of number_1 is equal to number_2
+print(f'{number_1} == {number_2}: {number_1 == number_2}')
+
+# != : return true if the value of number_1 is not equal to number_2
+print(f'{number_1} != {number_2}: {number_1 != number_2}')
+
+# > : return true if the value of number_1 is greater than number_2
+print(f'{number_1} > {number_2}: {number_1 > number_2}')
+
+# >= : = or >
+print(f'{number_1} >= {number_2}: {number_1 >= number_2}')
+
+# < : return true if the value of number_1 is less than number_2
+print(f'{number_1} < {number_2}: {number_1 < number_2}')
+
+# <= : = or <
+print(f'{number_1} <= {number_2}: {number_1 <= number_2}')
+
+
+userAge = 17
+
+if userAge > 18:
+    print('you can play the game')
+else:
+    print('you can\'t play the game')
